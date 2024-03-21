53dff288d8292c7b2e4296d41239bffe3f23f1a1
NBguys <gudwns520@gamil.com>
2024-03-21 14:18:54 +0900

    매개 변수 추가 하여 페이지 이동 추가

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,16 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs'; 
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
+  pramsSubscription: Subscription;
 
-  constructor() { }
+  constructor(private router: ActivatedRoute) { }
 
   ngOnInit() {
+    this.user = {
+      id: this.router.snapshot.params['id'],
+      name: this.router.snapshot.params['name']
+    };
+    this.pramsSubscription = this.router.params.subscribe(
+      (params: Params) => {
+        this.user.id = params['id'];
+        this.user.name = params['name'];
+      }
+    )
+  }
+
+  ngOnDestroy() {
+    this.pramsSubscription.unsubscribe();
   }
 
 }
